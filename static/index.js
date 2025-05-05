5e78d6a190c3df27b6adbcd62f5cf11b6e2f9478
Aritro Roy <140711141+airregalia@users.noreply.github.com>
2025-05-05 05:58:11 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict-depths", {
+            const response = await fetch("/predict-depths", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
